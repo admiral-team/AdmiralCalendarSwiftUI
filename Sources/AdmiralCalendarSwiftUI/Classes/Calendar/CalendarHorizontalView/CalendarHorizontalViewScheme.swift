3c50cf79128a56b6cc6ebcbb2cb2603a88c5694b
Borisov Kirill <>
2023-09-10 16:18:07 +0300

    Added package

---
+++ Sources/AdmiralCalendarSwiftUI/Classes/Calendar/CalendarHorizontalView/CalendarHorizontalViewScheme.swift
@@ -0,0 +1,49 @@
+//
+//  CalendarHorizontalViewScheme.swift
+//  AdmiralSwiftUI
+//
+//  Created on 15.09.2021.
+//
+
+import AdmiralCore
+import AdmiralTheme
+import SwiftUI
+
+/**
+ CalendarHorizontalViewScheme - the visual scheme of CalendarHorizontalView.
+ You can create a by specifying the following parameters in init:
+ - CalendarHorizontalViewScheme() - Initialize default CalendarHorizontalViewScheme with default themezation
+ # Example to create CalendarHorizontalViewScheme:
+ # Code
+ ```
+ let scheme = CalendarHorizontalViewScheme()
+ ```
+ */
+@available(iOS 14.0.0, *)
+public struct CalendarHorizontalViewScheme: AppThemeScheme {
+
+    // MARK: - Public Properties
+
+    /// The picker title color
+    public var pickerTitleColor: AColor
+
+    /// Header view scheme
+    public var headerViewScheme: CalendarHorizontalHeaderViewScheme
+
+    /// Week view scheme
+    public var calendarWeekViewScheme: CalendarWeekViewScheme
+
+    /// Days view scheme.
+    public var calendarViewCellColorScheme: CalendarViewCellColorScheme
+
+    // MARK: - Initializer
+
+    public init(theme: AppTheme = .default) {
+        pickerTitleColor = theme.colors.textPrimary
+
+        headerViewScheme = CalendarHorizontalHeaderViewScheme(theme: theme)
+        calendarWeekViewScheme = CalendarWeekViewScheme(theme: theme)
+        calendarViewCellColorScheme = CalendarViewCellColorScheme(theme: theme)
+    }
+
+}
